f2057c02a7138499a91bd11375daf238fc2474ea
ThewsyRum <102466085+ThewsyRum@users.noreply.github.com>
2026-07-19 09:41:27 -0300

    Modify _regen_section_header to include ptrbase
    Added ptrbase parameter to _regen_section_header function.

--- src/elffix/fix.cpp
+++ src/elffix/fix.cpp
@@ -116,7 +116,8 @@ template <typename Elf_Ehdr_Type, typename Elf_Shdr_Type,
           typename Elf_Rel_Type, bool isElf32>
 static void _regen_section_header(const Elf_Ehdr_Type *pehdr,
                                   const char *buffer, size_t len,
-                                  Elf_Shdr_Type g_shdr[SHDRS]) {
+                                  Elf_Shdr_Type g_shdr[SHDRS],
+                                  uint64_t ptrbase) {
   Elf_Phdr_Type lastLoad = {0};
   Elf_Phdr_Type *phdr = (Elf_Phdr_Type *)(buffer + pehdr->e_phoff);
   int ph_num = pehdr->e_phnum;
